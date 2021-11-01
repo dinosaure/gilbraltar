e359cb1a956280cfd07f89ee8ee5856b8384a23c
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-11-01 23:06:49 +0100

    Fix a bit the compilation

--- kernel/lib/kernel.c
+++ kernel/lib/kernel.c
@@ -1,5 +1,7 @@
 #include "io.h"
+#include "log.h"
 #include "mem.h"
+#include "crt_init.h"
 
 static char *unused_args[] = { "mirage", NULL };
 static uintptr_t sp_at_start;
@@ -8,8 +10,15 @@ extern void _nolibc_init(uintptr_t heap_start, size_t heap_size);
 extern void caml_startup(char **);
 
 void main() {
+  crt_init_ssp();
+
   uintptr_t heap_start;
   size_t    heap_size;
+
+  log(INFO, " _____ _ _ _           _ _           \n");
+  log(INFO, "|   __|_| | |_ ___ ___| | |_ ___ ___ \n");
+  log(INFO, "|  |  | | | . |  _| .'| |  _| .'|  _|\n");
+  log(INFO, "|_____|_|_|___|_| |__,|_|_| |__,|_|  \n");
 
   uart_init();
   mem_init();
