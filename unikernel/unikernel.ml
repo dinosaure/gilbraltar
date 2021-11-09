d0494bc4b29d4a61cc9cdec3aefd6fa8e57aa27e
Calascibetta Romain <romain.calascibetta@gmail.com>
2021-11-09 20:13:27 +0100

    Fix unikernel.ml

--- unikernel/unikernel.ml
+++ unikernel/unikernel.ml
@@ -1,5 +1,5 @@
 module Make (Console : Mirage_console.S) = struct
-  let loet console fmt = Fmt.kstr (fun str -> Console.log console str) fmt
+  let log console fmt = Fmt.kstr (fun str -> Console.log console str) fmt
 
   let start console =
     log console "Hello World from MirageOS!"
