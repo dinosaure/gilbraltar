7a5e754f3f6931d6c0b773bb20da954bb0f81263
Lucas Pluvinage <lucas@tarides.com>
2021-12-14 18:24:46 +0100

    move to EL1

--- kernel/lib/kernel.c
+++ kernel/lib/kernel.c
@@ -7,6 +7,7 @@
 
 extern void _nolibc_init(uintptr_t heap_start, size_t heap_size);
 extern void caml_startup(char **);
+extern int get_el();
 static char* args[] = { "gi(l)braltar", NULL };
 
 void _start_c() {
@@ -22,6 +23,7 @@ void _start_c() {
   log(INFO, "|   __|_| | |_ ___ ___| | |_ ___ ___ \n");
   log(INFO, "|  |  | | | . |  _| .'| |  _| .'|  _|\n");
   log(INFO, "|_____|_|_|___|_| |__,|_|_| |__,|_|  \n");
+  log(INFO, "EL:%d\n", get_el());
 
   mem_init();
   mem_lock_heap(&heap_start, &heap_size);
