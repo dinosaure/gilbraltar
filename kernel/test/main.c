64feaed443af8edd1156923a21b060bdce555bf8
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-12-15 18:03:16 +0100

    lint

--- kernel/test/main.c
+++ kernel/test/main.c
@@ -16,31 +16,25 @@ void _nolibc_init() {
 void uart_write_text(char *buffer);
 void uart_drain_output_queue();
 
-
 long mclock(void);
 
 void caml_startup(){
-    uart_write_text("Hello.\n");
+    uart_write_text("Hello World!.\n");
     uart_drain_output_queue();
 
     long clock = mclock();
 
-
     int data[0x1000000];
 
-    for (int i = 0; i < 0x1000000; i++) {
+    for (int i = 0; i < 0x1000000; i++)
         data[i] = i;
-    }
 
     int sum = 0;
 
-    for (int i = 0; i < 0x1000000; i++) {
-        if (i % 10000 == 0) {
+    for (int i = 0; i < 0x1000000; i++)
+        if (i % 10000 == 0)
             sum += data[i];
-        } 
-    }
 
-    log(INFO, "End. %d ms\n", ((mclock() - clock)) / 1000000);
+    log(INFO, "> %ld ms.\n", ((mclock() - clock)) / 1000000);
     uart_drain_output_queue();
-
 }
