c4c0789970b29a3ab0bccc7a85b6af4eb27dcadd
Lucas Pluvinage <lucas@tarides.com>
2021-12-16 18:01:28 +0100

    Support for IRQs

--- kernel/lib/interrupts.c
+++ kernel/lib/interrupts.c
@@ -1,3 +1,4 @@
+#include "interrupts.h"
 #include "log.h"
 #include "io.h"
 
@@ -8,4 +9,23 @@ void show_invalid_entry_message(int type, int esr_el1, int elr_el1, int sp) {
     while(1) {
         __asm__("wfi");
     }
+}
+
+void (*interrupt_handler)() = NULL;
+
+void register_interrupt_handler(void (* v_interrupt_handler)()) {
+    interrupt_handler = v_interrupt_handler;
+}
+
+void interrupt_handle_el1_irq() {
+    log(INFO, "IRQ\n");
+    uart_drain_output_queue();
+    if(interrupt_handler != NULL){
+        (*interrupt_handler)();
+    }
+}
+
+void yield() {
+    irq_enable();
+    __asm__("wfi");
 }
--- kernel/lib/interrupts.h
+++ kernel/lib/interrupts.h
@@ -3,3 +3,6 @@
 void irq_disable();
 void irq_enable();
 void irq_init_vectors();
+void yield();
+
+void register_interrupt_handler(void (*fn)());
